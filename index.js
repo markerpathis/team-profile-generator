bc01c3366de63ab81079957078da7128296f7aed
Parker Mathis <parkerjmathis@gmail.com>
2022-12-03 11:49:20 -0800

    Added pseudo code

---
+++ index.js
@@ -0,0 +1,13 @@
+// npm init and install inquirer v8.2.4
+// install jest as a dev dependency
+
+// Inquirer Flow
+// Objective:
+// Prompt and enter team members and their information, an HTML file is generated that displays a nicely formatted team roster based on the input
+// Requirements:
+// Step 1 - team managers name, employee ID, email address, and office number
+// Step 2 - option to add engineer or intern or finsih building my team
+/////////// engineer - name, employee ID, email, github username, then return to menu
+/////////// intern - name, employee ID, email, school, then return to menu
+/////////// when finished, the HTML file is generated
+
