4eff4425b86b4d7ce501cdfdd050469c71c6d22f
Parker Mathis <parkerjmathis@gmail.com>
2022-12-05 18:26:49 -0800

    Added additional tests

--- test/role.test.js
+++ test/role.test.js
@@ -2,23 +2,38 @@ const Manager = require("../lib/Manager");
 const Engineer = require("../lib/Engineer");
 const Intern = require("../lib/Intern");
 
-describe("Manager class", () => {
-  it("Manager class should return Manager as the role", () => {
-    const manager = new Manager("Parker", "1", "test@test.com", "55");
-    expect(manager.getRole()).toBe("Manager");
+describe("Manager", () => {
+  describe("Manager class", () => {
+    it("should create an object with Name, ID, Email, Office Number and Role if provided valid arguments", () => {
+      const manager = new Manager("Parker", "1", "test@test.com", "55");
+      expect(manager.getRole()).toBe("Manager");
+      expect(manager.getName()).toBe("Parker");
+      expect(manager.getId()).toBe("1");
+      expect(manager.getEmail()).toBe("test@test.com");
+    });
   });
 });
 
-describe("Engineer class", () => {
-  it("Engineer class should return Engineer as the role", () => {
-    const engineer = new Engineer("Elliot Alderson", "2", "elliott@allsafe.com", "elliotalderson");
-    expect(engineer.getRole()).toBe("Engineer");
+describe("Engineer", () => {
+  describe("Engineer class", () => {
+    it("should create an object with Name, ID, Email, Office Number and Role if provided valid arguments", () => {
+      const engineer = new Engineer("Elliot Alderson", "2", "elliott@allsafe.com", "elliotalderson");
+      expect(engineer.getRole()).toBe("Engineer");
+      expect(engineer.getName()).toBe("Elliot Alderson");
+      expect(engineer.getId()).toBe("2");
+      expect(engineer.getEmail()).toBe("elliott@allsafe.com");
+    });
   });
 });
 
-describe("Intern class", () => {
-  it("Intern class should return Intern as the role", () => {
-    const intern = new Intern("Billy McMahon", "3", "billy@google.com", "University of Phoenix");
-    expect(intern.getRole()).toBe("Intern");
+describe("Intern", () => {
+  describe("Intern class", () => {
+    it("should create an object with Name, ID, Email, Office Number and Role if provided valid arguments", () => {
+      const intern = new Intern("Billy McMahon", "3", "billy@google.com", "University of Phoenix");
+      expect(intern.getRole()).toBe("Intern");
+      expect(intern.getName()).toBe("Billy McMahon");
+      expect(intern.getId()).toBe("3");
+      expect(intern.getEmail()).toBe("billy@google.com");
+    });
   });
 });
