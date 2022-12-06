94e1b10357630bbfd99cbaec33d651177774009a
Parker Mathis <parkerjmathis@gmail.com>
2022-12-05 18:14:40 -0800

    Added Role tests for Engineer and Intern

--- test/role.test.js
+++ test/role.test.js
@@ -1,8 +1,24 @@
-const Manager = require("../index");
+const Manager = require("../lib/Manager");
+const Engineer = require("../lib/Engineer");
+const Intern = require("../lib/Intern");
 
 describe("Manager class", () => {
-  it("should return Manager as the role when Manager is called", () => {
+  it("Manager class should return Manager as the role", () => {
     const manager = new Manager("Parker", "1", "test@test.com", "55");
     expect(manager.getRole()).toBe("Manager");
+  });
+});
+
+describe("Engineer class", () => {
+  it("Engineer class should return Engineer as the role", () => {
+    const engineer = new Engineer("Elliot Alderson", "2", "elliott@allsafe.com", "elliotalderson");
+    expect(engineer.getRole()).toBe("Engineer");
+  });
+});
+
+describe("Intern class", () => {
+  it("Intern class should return Intern as the role", () => {
+    const intern = new Intern("Billy McMahon", "3", "billy@google.com", "University of Phoenix");
+    expect(intern.getRole()).toBe("Intern");
   });
 });
