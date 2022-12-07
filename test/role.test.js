7a7e15e378792c048c8173feecb5b43e240bc1b1
Parker Mathis <parkerjmathis@gmail.com>
2022-12-06 16:49:13 -0800

    Added test for Employee

--- test/role.test.js
+++ test/role.test.js
@@ -1,6 +1,19 @@
+const Employee = require("../lib/Employee");
 const Manager = require("../lib/Manager");
 const Engineer = require("../lib/Engineer");
 const Intern = require("../lib/Intern");
+
+describe("Employee", () => {
+  describe("Employee class", () => {
+    it("should create an object with Name, ID, Email, and Role if provided valid arguments", () => {
+      const employee = new Employee("Parker", "1", "test@test.com");
+      expect(employee.getRole()).toBe("Employee");
+      expect(employee.getName()).toBe("Parker");
+      expect(employee.getId()).toBe("1");
+      expect(employee.getEmail()).toBe("test@test.com");
+    });
+  });
+});
 
 describe("Manager", () => {
   describe("Manager class", () => {
@@ -10,18 +23,20 @@ describe("Manager", () => {
       expect(manager.getName()).toBe("Parker");
       expect(manager.getId()).toBe("1");
       expect(manager.getEmail()).toBe("test@test.com");
+      expect(manager.managerOffice).toBe("55");
     });
   });
 });
 
 describe("Engineer", () => {
   describe("Engineer class", () => {
-    it("should create an object with Name, ID, Email, Office Number and Role if provided valid arguments", () => {
+    it("should create an object with Name, ID, Email, School and Role if provided valid arguments", () => {
       const engineer = new Engineer("Elliot Alderson", "2", "elliott@allsafe.com", "elliotalderson");
       expect(engineer.getRole()).toBe("Engineer");
       expect(engineer.getName()).toBe("Elliot Alderson");
       expect(engineer.getId()).toBe("2");
       expect(engineer.getEmail()).toBe("elliott@allsafe.com");
+      expect(engineer.engineerUsername).toBe("elliotalderson");
     });
   });
 });
@@ -34,6 +49,7 @@ describe("Intern", () => {
       expect(intern.getName()).toBe("Billy McMahon");
       expect(intern.getId()).toBe("3");
       expect(intern.getEmail()).toBe("billy@google.com");
+      expect(intern.internSchool).toBe("University of Phoenix");
     });
   });
 });
